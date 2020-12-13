90cff386af9e47eb9e863a1e6837a20f7dc10961
ringodev <ringodev@protonmail.com>
2020-12-13 20:25:53 +0100

    initialized spring project

---
+++ backend/src/main/java/com/se/corona/Controller.java
@@ -0,0 +1,9 @@
+package com.se.corona;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("patients")
+public class Controller {
+}
