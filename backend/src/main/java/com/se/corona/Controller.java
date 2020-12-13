a64ea75a8b34e98bde76c6d779539ce23b86e178
ringodev <ringodev@protonmail.com>
2020-12-13 20:57:13 +0100

    added first swagger file

--- backend/src/main/java/com/se/corona/Controller.java
+++ backend/src/main/java/com/se/corona/Controller.java
@@ -1,9 +1,33 @@
 package com.se.corona;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+import java.util.List;
 
 @RestController
 @RequestMapping("patients")
 public class Controller {
+
+    @PostMapping("/")
+    void postPatient(@RequestBody Patient patient){
+
+    }
+
+    static class Patient{
+        Date dateInfect;
+        Date dateEnd;
+        Date Birthday;
+        Gender gender;
+        List<String> preconditions;
+        String district;
+        String state;
+    }
+
+    enum Gender{
+        W,M,U
+    }
 }
