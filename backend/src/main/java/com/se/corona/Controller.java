97e40100909a5eec76be3104c6364ccd6c2718d0
ringodev <ringodev@protonmail.com>
2020-12-23 11:31:46 +0100

    changed documentation

--- backend/src/main/java/com/se/corona/Controller.java
+++ backend/src/main/java/com/se/corona/Controller.java
@@ -1,18 +1,21 @@
 package com.se.corona;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("patients")
+@RequestMapping("patient")
 public class Controller {
 
     PatientRepository patientRepository;
+    Logger logger = LoggerFactory.getLogger(Controller.class);
 
     @Autowired
     Controller(PatientRepository patientRepository) {
@@ -20,28 +23,42 @@ public class Controller {
 
     }
 
-    @PostMapping("/")
+    @PostMapping("")
     void postPatient(@RequestBody Patient patient) {
 
     }
 
-    @GetMapping("/")
-    List<Patient> getPatients() {
-        return patientRepository.findAll();
+    @GetMapping("")
+    List<Patient> getPatients(@RequestParam(required = false) Long startBefore, @RequestParam(required = false) Long startAfter, @RequestParam(required = false) Long endBefore, @RequestParam(required = false) Long endAfter) {
+        List<Patient> patients = patientRepository.findAll();
+        if (startBefore != null) {
+            patients = patients.stream().filter(p -> p.dateInfect <= startBefore).collect(Collectors.toList());
+        }
+        if (startAfter != null) {
+            patients = patients.stream().filter(p -> p.dateInfect >= startAfter).collect(Collectors.toList());
+        }
+        if (endBefore != null) {
+            patients = patients.stream().filter(p -> p.dateEnd <= endBefore).collect(Collectors.toList());
+        }
+        if (endAfter != null) {
+            patients = patients.stream().filter(p -> p.dateInfect >= endAfter).collect(Collectors.toList());
+        }
+        return patients;
     }
 
     @PutMapping("/{id}")
     ResponseEntity<Object> updatePatient(@RequestBody Patient patient, @PathVariable String id) {
-        if(this.patientRepository.existsById(id)) {
+        if (this.patientRepository.existsById(id)) {
             patient.setId(id);
             this.patientRepository.save(patient);
             return new ResponseEntity<>(patient, HttpStatus.OK);
-        }
-        else return new ResponseEntity<>("Patient with id "+ id + " didn't exist in DB.",HttpStatus.BAD_REQUEST);
+        } else return new ResponseEntity<>("Patient with id " + id + " didn't exist in DB.", HttpStatus.BAD_REQUEST);
     }
 
     @DeleteMapping("/{id}")
     void deletePatient(@PathVariable String id) {
         this.patientRepository.deleteById(id);
     }
+
+
 }
--- backend/src/main/java/com/se/corona/Patient.java
+++ backend/src/main/java/com/se/corona/Patient.java
@@ -1,7 +1,5 @@
 package com.se.corona;
 
-import net.bytebuddy.dynamic.loading.InjectionClassLoader;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
