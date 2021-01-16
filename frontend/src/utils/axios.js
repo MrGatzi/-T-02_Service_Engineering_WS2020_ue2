780b90a8443efef3ccc4a0a6e72ca8aba6d0dca4
ringodev <ringodev@protonmail.com>
2021-01-16 12:49:22 +0100

    changed dbs setting to not delete on startup

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -2,7 +2,12 @@ import axios from "axios";
 
 const instance = axios.create({
 
-    baseURL: "http://localhost:8080/"
+    baseURL: "http://localhost:8080/",
+    headers: {
+        common: {
+            'Authorization': 'Bearer ' + localStorage.getItem('CognitoIdentityServiceProvider.4hu5e747hsoa3djebroq73lh7d.ringodev.idToken')
+        }
+    }
 });
 
 export default instance;
